d60ac2c78f9dcea54927a4572f4e8a5556732a20
Hy <nguyengiahy2911@gmail.com>
2021-08-16 00:06:07 +0700

    add problems

---
+++ Codeforces/Heap/SPOJ-the_lazy_programmer.py
@@ -0,0 +1,31 @@
+from heapq import heappop, heappush
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    tasks = []
+    for i in range(n):
+        a, b, d = map(int, input().split())
+        tasks.append((d, a, b))
+
+    tasks.sort()
+
+    time = money = 0
+    pq = []
+    for i in range(n):
+        d, a, b = tasks[i]
+        time += b
+        heappush(pq, (-a, b, d))
+        while time > d:
+            top_a, top_b, top_d = heappop(pq)
+            if top_b > time - d:
+                money += (time - d) / -top_a
+                top_b -= time - d
+                time = d
+                heappush(pq, (top_a, top_b, top_d))
+            else:
+                money += top_b / -top_a
+                time -= top_b
+                top_b = 0
+
+    print("%.2f" % money)
